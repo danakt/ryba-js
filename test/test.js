c6ee8785dd195a2068d88cb8eab38511c8159181
Danakt Frost <mail@danakt.ru>
2017-03-25 11:16:31 +0300

    Тестовый скрипт

---
+++ test/test.js
@@ -0,0 +1,19 @@
+const expect = require('chai').expect
+const ryba   = require('../')
+
+// Генерация текста ------------------------------------------------------------
+describe('Генерация текста', () => {
+    let sentenceExp = /[А-Я][^.?!]+[.?!]/g
+
+    it('1 предложение', () => {
+        expect(ryba().match(sentenceExp)).to.have.lengthOf(1)
+    })
+
+    it('10 предложений', () => {
+        expect(ryba(10).match(sentenceExp)).to.have.lengthOf(10)
+    })
+
+    it('10000 предложений', () => {
+        expect(ryba(1e4).match(sentenceExp)).to.have.lengthOf(1e4)
+    })
+})
