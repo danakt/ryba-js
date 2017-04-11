40699590d2f080185f2b12ecd16a1c1403073a6b
Danakt <danakt_frost@mail.ru>
2017-04-11 12:45:20 +0300

    Адаптация под es5

--- test/test.js
+++ test/test.js
@@ -1,19 +1,19 @@
-const expect = require('chai').expect
-const ryba   = require('../')
+var expect = require('chai').expect
+var ryba   = require('../')
 
 // Генерация текста ------------------------------------------------------------
-describe('Генерация текста', () => {
-    let sentenceExp = /[А-Я][^.?!]+[.?!]/g
+describe('Генерация текста', function() {
+    var sentenceExp = /[А-Я][^.?!]+[.?!]/g
 
-    it('1 предложение', () => {
+    it('1 предложение', function() {
         expect(ryba().match(sentenceExp)).to.have.lengthOf(1)
     })
 
-    it('10 предложений', () => {
+    it('10 предложений', function() {
         expect(ryba(10).match(sentenceExp)).to.have.lengthOf(10)
     })
 
-    it('10000 предложений', () => {
+    it('10000 предложений', function() {
         expect(ryba(1e4).match(sentenceExp)).to.have.lengthOf(1e4)
     })
 })
