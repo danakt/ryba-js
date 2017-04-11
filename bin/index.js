25c1a201eb63d1849b25da330ad3487c6c8ced26
Danakt <danakt_frost@mail.ru>
2017-04-11 12:32:51 +0300

    add babel

--- bin/index.js
+++ bin/index.js
@@ -1,28 +1,30 @@
-const fs        = require('fs')
-const getText   = require('text-generator-core')
-const path      = require('path')
+'use strict';
 
-const templates = require('./templates')
-const flections = require('./patterns')
+var fs = require('fs');
+var getText = require('text-generator-core');
+var path = require('path');
+
+var templates = require('./templates');
+var flections = require('./patterns');
 
 // Списки слов -----------------------------------------------------------------
-let links = {
-    НАЧ_ВВОД:   [ require('../links/нач_ввод') ],
-    ВВОД:       [ require('../links/ввод') ],
-    СУЩ:        [ require('../links/сущ') ],
-    ГЛАГ:       [ require('../links/глаг') ],
-    ПРИЛ:       [ require('../links/прил') ],
-    ДЕЕПРИЧ:    [ require('../links/дееприч') ],
-    КРАТК_ПРИЛ: [ require('../links/кратк_прил') ],
-    УТВЕРЖД:    [ require('../links/утвержд') ],
-}
+var links = {
+    НАЧ_ВВОД: [require('./links/нач_ввод')],
+    ВВОД: [require('./links/ввод')],
+    СУЩ: [require('./links/сущ')],
+    ГЛАГ: [require('./links/глаг')],
+    ПРИЛ: [require('./links/прил')],
+    ДЕЕПРИЧ: [require('./links/дееприч')],
+    КРАТК_ПРИЛ: [require('./links/кратк_прил')],
+    УТВЕРЖД: [require('./links/утвержд')]
+};
 
 // Экспорт ---------------------------------------------------------------------
-let options = { links, templates, flections }
-module.exports = num => {
+var options = { links: links, templates: templates, flections: flections };
+module.exports = function (num) {
     if (num == null) {
-        num = 1
+        num = 1;
     }
 
-    return getText(Math.max(num, 0), options)
-}
+    return getText(Math.max(num, 0), options);
+};
