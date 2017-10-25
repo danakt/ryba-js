ed334fd85eec687fcaf0b340dd904d474587f6d8
Danakt Frost <mail@danakt.ru>
2017-10-26 00:17:15 +0300

    Добавлена функция маппинга объекта в utils

--- src/utils.ts
+++ src/utils.ts
@@ -24,7 +24,8 @@ export function toPairs<T>(obj: { [prop: string]: T }): [string, T][] {
 
 /**
  * Возвращает true или false случайным образом
- * @param {?number} [chance=0.5] Шанс выпадения true
+ * @param  {?number} [chance=0.5] Шанс выпадения true
+ * @return {boolean}
  */
 export function randomBoolean(chance: number = 1 / 2) {
   return Math.random() < chance
@@ -39,4 +40,22 @@ export function getRandomItemFromArray<T>(arr: T[]): T {
   const randomItem: T = arr[randomIndex]
 
   return randomItem
+}
+
+/**
+ * Маппинг объекта
+ * @param  {Function} fn  Маппер
+ * @param  {object}   obj Объект
+ * @return {object}
+ */
+export function mapObject<T, TResult>(
+  fn:  (value: T, key: string, obj?: any) => TResult,
+  obj: { [prop: string]: T }
+): { [index: string]: TResult } {
+  return Object.keys(obj).reduce((acc, key) => {
+    return {
+      ...acc,
+      [key]: fn(obj[key], key, obj)
+    }
+  }, {})
 }
