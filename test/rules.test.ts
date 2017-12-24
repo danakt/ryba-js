8737cd7d1b25ba453fd4f1cd733fd436e1906362
Danakt Frost <danakt_frost@mail.ru>
2017-12-24 23:50:43 +0300

    Добавлен todo о добавлении проверки структура правил трансформации

---
+++ test/rules.test.ts
@@ -0,0 +1,12 @@
+import { expect } from 'chai'
+import { transformRules } from '../src/statics/rules'
+
+/**
+ * @todo Сделать проверку структуры
+ * @code
+ *   describe('Проверка правил трансформации', () => {
+ *     it('Проверка струкутры', () => {
+ *       // ...
+ *     })
+ *   })
+ */
